corrected folder name for scaling factor

test_image.py
@@ -16,7 +16,7 @@ from model import Net
 from torchvision import transforms
 from data_utils import DatasetFromFolder
 
-UPSCALE_FACTOR = 3
+UPSCALE_FACTOR = 2
 
 trainset = DatasetFromFolder('data/train', upscale_factor=UPSCALE_FACTOR, input_transform=transforms.ToTensor(),
                                   target_transform=transforms.ToTensor())
@@ -25,12 +25,12 @@ trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
                                           shuffle=True, num_workers=2)
 
 " load net "
-PATH = 'jortTrained.pth'
+PATH = 'rikTrained.pth'
 net = Net(UPSCALE_FACTOR)
 net.load_state_dict(torch.load(PATH))
 
-path = 'data/train/SRF_3/data/'
-image_name = '2007_000033.jpg'
+path = 'data/train/scaling_factor_2/data/'
+image_name = 'tt12.bmp'
 
 img = Image.open(path + image_name).convert('YCbCr')
 y, cb, cr = img.split()

train.py
@@ -14,7 +14,7 @@ import torch.nn as nn
 
 
 if __name__ == "__main__":
-    UPSCALE_FACTOR = 3
+    UPSCALE_FACTOR = 2
     net = Net(upscale_factor=UPSCALE_FACTOR)
     print(net)
     
@@ -48,7 +48,7 @@ if __name__ == "__main__":
     optimizer = torch.optim.Adam(net.parameters(), lr=1e-2)
     
     " train net "
-    for epoch in range(2):  # loop over the dataset multiple times
+    for epoch in range(10):  # loop over the dataset multiple times
     
         running_loss = 0.0
         for i, data in enumerate(trainloader, 0):
@@ -73,5 +73,5 @@ if __name__ == "__main__":
     
     print('Finished Training')
     " save "
-    PATH = './jortTrained.pth'
+    PATH = './rik_newTrained.pth'
     torch.save(net.state_dict(), PATH)